e29421f86d0b4985550a33d755b8eb05b437383d
Acherium <shinsu02@naver.com>
2023-12-23 22:27:44 +0900

    웹페이지 디자인 데모 업데이트

--- lyraux-demo/scripts/demo.js
+++ lyraux-demo/scripts/demo.js
@@ -24,7 +24,7 @@ const modalList = {
             {
                 "icon": "deny",
                 "nametag": "취소",
-                "onclick": "null"
+                "onclick": null
             }
         ]
     },
@@ -40,7 +40,7 @@ const modalList = {
             {
                 "icon": "deny",
                 "nametag": "취소",
-                "onclick": "null"
+                "onclick": null
             }
         ]
     }
@@ -53,10 +53,12 @@ function showModal(e) {
 
     const modalBackground = document.createElement("div");
     modalBackground.classList.add("lyra-modal-background");
+    modalBackground.classList.add("lyra-ani-window-fade");
     modalBackground.setAttribute("onclick", "closeModal(this.parentNode);");
 
     const modalArea = document.createElement("div");
     modalArea.classList.add("lyra-modal-area");
+    modalArea.classList.add("lyra-ani-window-jumpup");
 
     const modalWrap = document.createElement("div");
     modalWrap.classList.add("lyra-modal-wrap");
@@ -92,7 +94,7 @@ function showModal(e) {
             _button.setAttribute("onclick", `${b.onclick}`);
         } else {
             _button.setAttribute("onclick", "closeModal(this.parentNode.parentNode.parentNode.parentNode);");
-        }
+        };
 
         _button.append(_buttonCover);
         _button.append(_buttonIcon);
@@ -107,8 +109,17 @@ function showModal(e) {
     modal.append(modalBackground);
     modal.append(modalArea);
     body.append(modal);
+
+    setTimeout(() => {
+        modalArea.classList.remove("lyra-ani-window-jumpup");
+        modalBackground.classList.remove("lyra-ani-window-fade");
+    }, 30);
 };
 
 function closeModal(e) {
-    body.removeChild(e);
+    e.querySelector(".lyra-modal-area").classList.add("lyra-ani-window-jumpup");
+    e.querySelector(".lyra-modal-background").classList.add("lyra-ani-window-fade");
+    setTimeout(() => {
+        body.removeChild(e);
+    }, 200);
 };
