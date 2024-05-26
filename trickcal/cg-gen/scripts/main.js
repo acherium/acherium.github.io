96f1700112cf0ac5b611708dc6e995289256e4a6
Acherium <shinsu02@naver.com>
2024-05-26 11:31:37 +0900

    슬라이드 위치 버그 수정

--- trickcal/cg-gen/scripts/main.js
+++ trickcal/cg-gen/scripts/main.js
@@ -3,7 +3,7 @@
         name: "Trickcal CG Scene Generator",
         author: "Acherium",
         contact: "acherium@pm.me",
-        version: "1.0.1011",
+        version: "1.0.1012",
         date: "24-05-26",
         watermark: false,
         isBeta: true
