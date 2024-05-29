2c708a3a52993ba8ecd1ef03f73a8ab5dcd2d737
Acherium <shinsu02@naver.com>
2024-05-29 19:13:08 +0900

    이미지 레이어 정렬순서 정리

--- trickcal/cg-gen/scripts/main.js
+++ trickcal/cg-gen/scripts/main.js
@@ -3,7 +3,7 @@
         name: "Trickcal CG Scene Generator",
         author: "Acherium",
         contact: "acherium@pm.me",
-        version: "1060",
+        version: "1061",
         date: "24-05-29",
         watermark: false,
         isBeta: true
